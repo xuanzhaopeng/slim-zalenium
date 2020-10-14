0a87ba0f19e2060d994646271ef9e5baa7839d23
xuanzhaopeng <zhaopengxuan@pvh.com>
2020-10-14 22:39:43 +0200

    Delete not used file

--- SECURITY.md
+++
@@ -1,1 +0,0 @@
-If you have discovered a security vulnerability, please email tech-security@zalando.de
--- push_image.sh
+++ push_image.sh
@@ -17,9 +17,9 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ] && [ -n "${TRAVIS_TAG}" ] && [ "${TRAVIS
 	docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
     echo "Logged in to docker with user '${DOCKER_USERNAME}'"
     echo "docker tag and docker push using TRAVIS_TAG=${TRAVIS_TAG}"
-    docker tag zalenium:${TRAVIS_TAG} xuanzhaopeng/slim-zalenium:${TRAVIS_TAG}
+    docker tag slim-zalenium:${TRAVIS_TAG} xuanzhaopeng/slim-zalenium:${TRAVIS_TAG}
     docker push xuanzhaopeng/slim-zalenium:${TRAVIS_TAG} | tee docker_push.log
-    docker tag zalenium:${TRAVIS_TAG} xuanzhaopeng/slim-zalenium:latest
+    docker tag slim-zalenium:${TRAVIS_TAG} xuanzhaopeng/slim-zalenium:latest
     docker push xuanzhaopeng/slim-zalenium:latest
 
 else
